7a981edabafb3e2ad5fcb48c24f2a92a0996f0d0
Natasha <natallinya@gmail.com>
2017-02-21 14:36:00 +0400

    Opencart

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+﻿<?php
+// HTTP
+define('HTTP_SERVER', 'http://begincscart.ru/admin/');
+define('HTTP_CATALOG', 'http://begincscart.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://begincscart.ru/admin/');
+define('HTTPS_CATALOG', 'http://begincscart.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/v/v79022ry/begincscart.ru/public_html/admin/');
+define('DIR_SYSTEM', '/home/v/v79022ry/begincscart.ru/public_html/system/');
+define('DIR_IMAGE', '/home/v/v79022ry/begincscart.ru/public_html/image/');
+define('DIR_LANGUAGE', '/home/v/v79022ry/begincscart.ru/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/v/v79022ry/begincscart.ru/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/v/v79022ry/begincscart.ru/public_html/system/config/');
+define('DIR_CACHE', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/v/v79022ry/begincscart.ru/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'v79022ry_openc');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'v79022ry_openc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://begincscart.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://begincscart.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/v/v79022ry/begincscart.ru/public_html/catalog/');
+define('DIR_SYSTEM', '/home/v/v79022ry/begincscart.ru/public_html/system/');
+define('DIR_IMAGE', '/home/v/v79022ry/begincscart.ru/public_html/image/');
+define('DIR_LANGUAGE', '/home/v/v79022ry/begincscart.ru/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/v/v79022ry/begincscart.ru/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/v/v79022ry/begincscart.ru/public_html/system/config/');
+define('DIR_CACHE', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/v/v79022ry/begincscart.ru/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'v79022ry_openc');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'v79022ry_openc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
